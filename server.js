c1563a0f9fe3167e5d0a35072dc74af36ef9848e
Eros <george.marr.cobc@gmail.com>
2017-10-15 17:35:02 +0100

    that is more than likely wrong

--- server.js
+++ server.js
@@ -12,3 +12,27 @@ server.FileName = "server.jar";
 
 server.maxRam = "";//need to change this
 server.minRam = "";//and this
+
+server.worldsDir = "../worlds/";
+server.pluginsDir = "../plugins/";
+server.configDir = "../settings/";
+
+server.start = function(cb){
+    if(server.proc == null){
+        server.proc = spawn('java',
+    [
+        "-Dcom.mojang.eula.agree=true",
+        "-Xms" + server.minRam,
+        "-Xmx" + server.maxRam,
+        "-jar", server.FileName,
+        "--world-dir", server.worldsDir,
+        "--plugins", server.pluginsDir,
+        "--bukkit-settings", server.configDir + "bukkit.yml",
+        "--command-settings", server.configDir + "commands.yml",
+        "--config", server.configDir + "config.yml",
+        "--spigot-settings", server.configDir + "spigot.yml"
+    ],{
+        cwd : server.path
+    })
+    }
+}
