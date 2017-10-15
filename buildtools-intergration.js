702c14fe717ea270d96939c1a0b655b3c6e3faea
Eros <george.marr.cobc@gmail.com>
2017-10-15 17:28:51 +0100

    this should work...

--- buildtools-intergration.js
+++ buildtools-intergration.js
@@ -22,5 +22,36 @@ buildtools.compile = function(cwd, filename, version, cb){
         if(log.indexOf("WARN") > -1)
             log = log.warn;
         console.log("BuildTools-ISSUE ".BTP, log);
-    })
+    });
+
+    BTP.stderr.on('data', function(data){
+        console.error("BuildTools-ISSUE".BTP, data.toString().replace("\n", "").error);
+    });
+    BTP.on('exit', cb);
+};
+
+buildtools.run = function(cb){
+    async.series([
+        function(callback){
+            console.log("Downloading latest version of buildtools".notifications);
+            buildtools.download(config.buildtools.path, config.buildtools.filename, function(){
+                console.log("BuildTools has been downloaded");
+                callback();
+            });
+        },
+
+        function (callback){
+            buildtools.compile(config.buildtools.path, config.buildtools.filename, "latest", function(){
+                console.log("Server has compiled the build tools".notifications);
+
+                fs.readdir(config.buildtools.path, function(err, files){
+                    var fn = files.find(function(element){
+                        return element.indexOf("spigot") !== -1;
+                    });
+                    fs.createReadStream(config.buildtools.path + filename).pipe(fs.createWriteStream(config.minecraftserv.path + config.minecraftserv.filename));
+                    console.log();
+                    callback();
+                });
+            }
+    ,], cb);
 }
